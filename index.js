bfe9922bc224fb878d65c0f4b16158b317b0ddb6
automathematical <vanleemputtenmathieu@gmail.com>
2023-11-18 15:45:19 +0100

    c

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-const showModal = () => {
-  const modal = document.getElementById('new-project-modal')
+const showModal = (id) => {
+  const modal = document.getElementById(id)
   modal.showModal()
 }
 
-//
+// this is the document ..
 const newProjectBtn = document.getElementById('new-project-btn')
-newProjectBtn.addEventListener('click', showModal)
+if (newProjectBtn) {
+  newProjectBtn.addEventListener('click', () => {
+    showModal('new-project-modal')
+  })
+} else {
+  console.log('New Projects button was not found')
+}
+
+const projectForm = document.getElementById('new-project-form')
+if (projectForm) {
+  projectForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const formData = new FormData(projectForm)
+    const project = {
+      name: formData.get('name'),
+      description: formData.get('description'),
+      status: formData.get('status'),
+      role: formData.get('role'),
+      finishDate: formData.get('finishDate'),
+    }
+    console.log(project)
+  })
+} else {
+  console.log('The Project form was not found')
+}
