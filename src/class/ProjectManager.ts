6d9f3d62b53857100ff070f4ef067f579607fbd3
automathematical <vanleemputtenmathieu@gmail.com>
2023-12-13 11:28:34 +0700

    c

--- src/class/ProjectManager.ts
+++ src/class/ProjectManager.ts
@@ -6,6 +6,14 @@ export class ProjectManager {
 
     constructor(container: HTMLElement) {
         this.ui = container
+        const project = this.newProject({
+            name: "Default Project",
+            description: "This is just a default app project",
+            status: "pending",
+            userRole: "architect",
+            finishDate: new Date()
+        })
+        project.ui.click()
     }
 
     newProject(data: IProject) {
